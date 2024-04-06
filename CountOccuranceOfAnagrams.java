50cccbbe5ccfde4a6c9941ba90f35ed122d8f8b5
abhishek farshwal <abhishekfarshwal2634@gmail.com>
2024-04-06 16:09:28 +0530

    changes made

--- CountOccuranceOfAnagrams.java
+++ CountOccuranceOfAnagrams.java
@@ -1,6 +1,25 @@
+import java.util.Arrays;
 
 public class CountOccuranceOfAnagrams {
     public static void main(String[] args) {
-       
+       String a = "abab";
+       String b = "abababbcdaabba";
+
+      int arr[] = new int[26],arr1[] = new int[26];
+      int r =0,l = 0,ans =0;
+
+      for(int i = 0; i < a.length();i++){
+        arr[a.charAt(i) -'a']++;
+      }
+
+      while(r < b.length()){
+        arr1[b.charAt(r) - 'a']++;
+        if(r - l + 1 == a.length()){
+            if(Arrays.equals(arr, arr1)) ans ++;
+            arr1[b.charAt(l++) - 'a'] --;
+        }
+        r++;
+      }
+      System.out.println(ans);
     }
 }
